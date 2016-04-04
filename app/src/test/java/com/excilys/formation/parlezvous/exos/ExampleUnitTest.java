ea69efac044afd1f1604ca4af278cb6ea433d547
Camille Vrod <cvrod@excilys.com>
2016-04-04 14:22:21 +0200

    feat(Exo2): Adding AndroidStudio Project

---
+++ app/src/test/java/com/excilys/formation/parlezvous/exos/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.excilys.formation.parlezvous.exos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
