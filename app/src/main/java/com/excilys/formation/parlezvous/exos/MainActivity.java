a172d620af54826cbd2c535006e0b42e7f9cfa7f
Camille Vrod <cvrod@excilys.com>
2016-04-04 14:17:30 +0200

    feat(Exo1): connexion page

---
+++ app/src/main/java/com/excilys/formation/parlezvous/exos/MainActivity.java
@@ -0,0 +1,53 @@
+package com.excilys.formation.parlezvous.exos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //Getting class name
+    private final String TAG = MainActivity.class.getSimpleName();
+    private EditText usernameField;
+    private EditText passwordField;
+    private Button flushButton;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate !");
+        usernameField = (EditText) findViewById(R.id.user);
+        passwordField = (EditText) findViewById(R.id.password);
+
+        flushButton = (Button) findViewById(R.id.viderButton);
+        sendButton = (Button) findViewById(R.id.envoyerButton);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy !");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause !");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume !");
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState !");
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState !");
+    }
+}
